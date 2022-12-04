c6f60208b39a099bf60c4c5b3600e145244631eb
Lou Vang <louvangdev@gmail.com>
2022-12-04 17:51:26 -0600

    Setup base views

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,15 +1,22 @@
 <?php
-  class Pages {
+  class Pages extends Controller {
     public function __construct(){
-      echo 'Pages.php controller ran...<br /><br />';
+
     }
 
     public function index(){
-      
+      $data = [
+        'title' => 'Welcome'
+      ];
+
+      $this->view('pages/index', $data);
     }
 
-    public function about($id){
-      echo 'This is about';
-      echo $id;
+    public function about(){
+      $data = [
+        'title' => 'About Us'
+      ];
+
+      $this->view('pages/about', $data);
     }
   }
